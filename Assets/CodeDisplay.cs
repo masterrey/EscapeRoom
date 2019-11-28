4612fd2920bafe07059dbd7bb9989bb0dc929935
Reinaldo <masterrey@gmail.com>
2019-11-27 21:02:41 -0300

    first

---
+++ Assets/CodeDisplay.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodeDisplay : MonoBehaviour
+{
+    public TextMesh display;
+    public string password;
+    bool first = true;
+    public GameObject objectToactive;
+    public Rigidbody rdbdoor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void setButtonKey(string btn)
+    {
+        if (first)
+        {
+            display.text = "";
+            first = false;
+        }
+        if (btn=="E"){
+            if (password == display.text)
+            {
+                display.text = "aberto";
+                if (objectToactive)
+                {
+                    objectToactive.SetActive(true);
+                }
+                if (rdbdoor)
+                {
+                    rdbdoor.isKinematic = false;
+                }
+
+            }
+            else
+            {
+                display.text = "errado";
+                first = true;
+            }
+            return;
+        }
+        if (btn == "C")
+        {
+            display.text = "";
+            return;
+        }
+        display.text += btn;
+    }
+}
---
+++ Assets/PointAndClick.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointAndClick : MonoBehaviour
+{
+    RaycastHit hit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetButton("Fire2"))
+        {
+            Cursor.visible = false;
+            transform.Rotate(new Vector3(0,Input.GetAxis("Mouse X"), 0), Space.World);
+            transform.Rotate(new Vector3(Input.GetAxis("Mouse Y"), 0, 0), Space.Self);
+        }
+        else
+        {
+            
+            Cursor.visible = true;
+        }
+        if (Input.GetButton("Fire1") && !Input.GetButton("Fire3"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray,out hit, 100))
+            {
+                print(hit.collider.name);
+                if (hit.collider.tag == "GameController")
+                {
+                    hit.collider.gameObject.GetComponent<Rigidbody>().AddForceAtPosition(transform.forward*10,hit.point);
+
+                }
+            }
+
+        }
+        if (Input.GetButton("Fire1") && Input.GetButton("Fire3"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                print(hit.collider.name);
+                if (hit.collider.tag == "GameController")
+                {
+                    hit.collider.gameObject.GetComponent<Rigidbody>().AddForceAtPosition(transform.forward * -10, hit.point);
+
+                }
+            }
+
+        }
+    }
+}
